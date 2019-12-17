ce445287140b29fb5bbfc8d90d29e07284891b00
codingame120 <codinggame120@gmail.com>
2019-12-17 16:03:47 +0100

    initial commit

--- pocetna.js
+++ pocetna.js
@@ -17,3 +17,26 @@ $(document).ready(function() {
       });
    });
 });
+
+
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+
+
+jQuery(document).ready(function() {
+      jQuery('.toggle-nav').click(function(e) {
+         jQuery(this).toggleClass('active');
+         jQuery('.menu ul').toggleClass('active');
+   
+         e.preventDefault();
+      });
+   });
