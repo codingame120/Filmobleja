521e50b4644f55960c4732e5929b2c4ce0a3c6f3
codingame120 <codinggame120@gmail.com>
2019-12-23 19:22:50 +0100

    dodate slike kao pozadine

---
+++ js/video.js
@@ -0,0 +1,23 @@
+
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+
+
+
+jQuery(document).ready(function() {
+      jQuery('.toggle-nav').click(function(e) {
+         jQuery(this).toggleClass('active');
+         jQuery('.menu ul').toggleClass('active');
+   
+         e.preventDefault();
+      });
+   });
