03fcbbe1fd507de4eafddf6b88ce90f82be3d6b4
codingame120 <codinggame120@gmail.com>
2020-01-02 16:55:15 +0100

    dodate serije

---
+++ js/serije.js
@@ -0,0 +1,32 @@
+
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+
+
+jQuery(document).ready(function() {
+      jQuery('.toggle-nav').click(function(e) {
+         jQuery(this).toggleClass('active');
+         jQuery('.menu ul').toggleClass('active');
+   
+         e.preventDefault();
+      });
+   });
+
+
+function Function() {
+  var x = document.getElementById("Demo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
